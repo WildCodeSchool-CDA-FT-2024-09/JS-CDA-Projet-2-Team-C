98c9ce6243dd0fd5961409d58844ecaa4e8f619f
Adri-bis <adrien.ma.16@hotmail.fr>
2024-11-25 16:54:46 +0100

    feat:add docker file for the client

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: '0.0.0.0', //  Listen on all network interfaces
+    port: 5173, // Port exposed in Docker
+    watch: {
+      usePolling: true
+    }
+  }
+});

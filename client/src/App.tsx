35de423b87601d114a190cef7614fa23e457bbaa
Ektoplasme <kainomel@hotmail.fr>
2024-11-27 15:44:03 +0100

    chore: adds env file

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,14 @@
+import { useRolesQuery } from './generated/graphql-types';
+
 export default function App() {
+  const { loading, data } = useRolesQuery();
+
+  if (loading) return <div>loading</div>;
+
   return (
     <>
       <h1 className="text-3xl font-bold underline">This is Chocolateam</h1>
+      {data?.roles}
     </>
   );
 }
--- core_api/src/database/dataSource.ts
+++ core_api/src/database/dataSource.ts
@@ -3,7 +3,7 @@ import { Role } from '../modules/role/role.entity';
 
 export const dataSource = new DataSource({
   type: 'postgres',
-  host: process.env.DB_HOST, // Nom de l'image associé à Postgres --name dans la commande
+  host: process.env.DB_HOST,
   port: Number(process.env.POSTGRES_PORT),
   username: 'postgres',
   password: process.env.POSTGRES_PASSWORD,

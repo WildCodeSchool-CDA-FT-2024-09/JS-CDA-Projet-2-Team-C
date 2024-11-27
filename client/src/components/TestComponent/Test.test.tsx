4f0ae07a9ca9311c2ae5a7c44c63eab72ce0a39d
Adri-bis <adrien.ma.16@hotmail.fr>
2024-11-27 10:56:41 +0100

    fix: fix test config, now run correctly client test and api test in global api

--- client/src/components/TestComponent/Test.test.tsx
+++ client/src/components/TestComponent/Test.test.tsx
@@ -1,12 +1,13 @@
 import { screen, render } from '@testing-library/react';
 import { describe, it, expect } from 'vitest';
+import '@testing-library/jest-dom';
 import Test from './Test';
 
 describe('Test de mon composant Test', () => {
   it('Should render a test', async () => {
-    await render(<Test test="Test du h1" />);
+    render(<Test test="Test du h1" />);
 
     const h1 = screen.getByRole('heading', { level: 1 });
-    expect(h1.textContent).toBe('Test du h1');
+    expect(h1).toHaveTextContent('Test du h1');
   });
 });

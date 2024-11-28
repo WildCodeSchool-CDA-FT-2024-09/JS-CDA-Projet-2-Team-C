3c8f90f0997b414884b39f0f6d19d4979bbd62cf
AkiraAcetonoshi <facilitateurmaker@gmail.com>
2024-11-28 11:22:52 +0100

    fix(backend tests): created global test script without workspaces and adapted dataSource to create testing sqlite database only for integration tests

--- core_api/jest.setup.ts
+++ core_api/jest.setup.ts
@@ -1,5 +1,6 @@
 import { beforeAll, afterAll } from '@jest/globals';
-import { dataSource } from './src/database/dataSource';
+import dataSource from './src/database/dataSource';
 
+// TODO : seed database
 beforeAll(async () => await dataSource.initialize());
 afterAll(() => dataSource.destroy());

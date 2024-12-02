338b2d48c8815f3b91e80d88ebd7c1ae35f3c1d6
AkiraAcetonoshi <facilitateurmaker@gmail.com>
2024-12-02 17:23:38 +0100

    setup(client) : installed react-router-dom and created Login page

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,14 +1,37 @@
 import { StrictMode } from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
 import { ApolloProvider } from '@apollo/client';
 import { client } from './services/client';
 import App from './App.tsx';
+import Login from './pages/Login/Login.tsx';
 import './index.css';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Login />
+      },
+      {
+        path: 'planning',
+        element: <h1>Planning</h1>
+      },
+      {
+        path: 'patient/:patientId/dossier',
+        element: <h1>Dossier</h1>
+      }
+    ]
+  }
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ApolloProvider client={client}>
-      <App />
+      <RouterProvider router={router} />
     </ApolloProvider>
   </StrictMode>
 );

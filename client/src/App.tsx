aba25367d8c3e651426f35f57816ee9099df2c96
Ektoplasme <kainomel@hotmail.fr>
2024-12-02 15:55:29 +0100

    feat: add daisyUI and test in App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,10 +8,23 @@ export default function App() {
   if (loading) return <div>loading</div>;
 
   return (
-    <section className="self-center">
+    <section className="flex w-80 flex-col place-items-center place-self-center">
       <h1 className="text-center text-4xl font-medium">Agenda Médical</h1>
-      <img className="place-self-center" src={Logo} />
-      <h2 className="text-center text-4xl">Connexion</h2>
+      <section className="flex w-full flex-col place-items-center gap-4">
+        <img className="place-self-center" src={Logo} />
+        <h2 className="text-center text-4xl">Connexion</h2>
+        <input
+          type="text"
+          placeholder="Email ..."
+          className="input input-bordered input-primary w-full max-w-xs rounded-full"
+        />
+        <input
+          type="text"
+          placeholder="Mot de passe ..."
+          className="input input-bordered input-primary w-full max-w-xs rounded-full"
+        />
+        <button className="btn w-40 bg-primary">CONNEXION</button>
+      </section>
     </section>
   );
 }

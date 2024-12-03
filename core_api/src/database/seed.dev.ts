7fc18afbabd0b36297e5d2e23deb853001d4a490
Thomas Allen <thomas-allen_student2021@wilder.school>
2024-12-03 14:48:25 +0100

    api/dev-seed: add generation of fake patients and consultations

--- core_api/src/database/seed.dev.ts
+++ core_api/src/database/seed.dev.ts
@@ -96,7 +96,7 @@ import dataSource from './dataSource';
     // Add min one milisecond tick to avoid duplicate emails
     async function fakeEmail(): Promise<string> {
       const time = Date.now();
-      await new Promise((resolve) => setTimeout(resolve, 1));
+      await new Promise((resolve) => setTimeout(resolve, 3));
       return `fake${time}@fake.com`;
     }
 
@@ -233,11 +233,14 @@ import dataSource from './dataSource';
       )
       .join(', ');
 
-    await queryRunner.manager.query(`
+    const secretaryResult = await queryRunner.manager.query(`
       INSERT INTO "user"
       (firstname, lastname, email, password, "roleId", "genderId", "departmentId")
       VALUES ${secretaryValues}
+      RETURNING id
     `);
+
+    const secretaryIds = secretaryResult.map((item: { id: number }) => item.id);
 
     // 3. WORKING HOURS
     type WorkdayHours = {
@@ -280,14 +283,137 @@ import dataSource from './dataSource';
       )
       .join(', ');
 
-    // 4. PATIENTS
-    // TODO
-
     await queryRunner.manager.query(`
-      INSERT INTO "working_hours"
-      ("doctorId", "weekday", "startTime", "endTime")
-      VALUES ${workingHoursValues}
+        INSERT INTO "working_hours"
+        ("doctorId", "weekday", "startTime", "endTime")
+        VALUES ${workingHoursValues}
+        `);
+
+    // 4. PATIENTS
+    function generateFakeSSN(): string {
+      const ssn = Math.floor(Math.random() * 1000000000000000).toString();
+      return ssn;
+    }
+
+    const patients = [
+      {
+        firstname: 'Pedro',
+        lastname: 'Patient',
+        email: await fakeEmail(),
+        ssn: generateFakeSSN(),
+        town: 'Townsville',
+        postcode: '12345',
+        dateOfBirth: '1990-04-21',
+        genderId: genderIdMap.male
+      },
+      {
+        firstname: 'Penelope',
+        lastname: 'Patient',
+        email: await fakeEmail(),
+        ssn: generateFakeSSN(),
+        town: 'Townsville',
+        postcode: '12345',
+        dateOfBirth: '1980-10-17',
+        genderId: genderIdMap.female
+      },
+      {
+        firstname: 'Pantagruel',
+        lastname: 'Patient',
+        email: await fakeEmail(),
+        ssn: generateFakeSSN(),
+        town: 'Townsville',
+        postcode: '12345',
+        dateOfBirth: '1970-01-05',
+        genderId: genderIdMap.male
+      },
+      {
+        firstname: 'Persephone',
+        lastname: 'Patient',
+        email: await fakeEmail(),
+        ssn: generateFakeSSN(),
+        town: 'Townsville',
+        postcode: '12345',
+        dateOfBirth: '2000-09-30',
+        genderId: genderIdMap.female
+      }
+    ];
+
+    const patientValues = patients
+      .map(
+        (patient) =>
+          `('${patient.firstname}', '${patient.lastname}', '${patient.email}', '${patient.ssn}', '${patient.town}', '${patient.postcode}', '${patient.dateOfBirth}', ${patient.genderId})`
+      )
+      .join(', ');
+
+    const patientResult = await queryRunner.manager.query(`
+      INSERT INTO "patient"
+      (firstname, lastname, email, ssn, town, postcode
+      , "dateOfBirth", "genderId")
+      VALUES ${patientValues}
+      RETURNING id
     `);
+
+    const patientIds = patientResult.map((item: { id: number }) => item.id);
+
+    // 5. CONSULTATIONS
+    type Consultation = {
+      description: string;
+      consultationDate: string;
+      startTime: string;
+      durationMinutes: number;
+      subjectId: number;
+      doctorId: number;
+      authorId: number;
+      patientId: number;
+    };
+
+    // IMPORTANT NOTE - these start times are currently hardcoded to work with 4 fake patients and to fit within possible fake doctor working times on a single day. This is to avoid having to do lengthy checks of consistency that randomly generated consultations don't happen at the same time with the same doctor.
+    const consultationStartTimes = ['12:00', '12:30', '13:00', '13:30'];
+
+    const consultations: Consultation[] = patientIds.map(
+      (patientId: number, index: number): Consultation => {
+        const doctorId =
+          doctorIds[Math.floor(Math.random() * doctorIds.length)];
+        const authorId =
+          secretaryIds[Math.floor(Math.random() * secretaryIds.length)];
+        const subjectId =
+          Math.floor(Math.random() * consultationSubjects.length) + 1;
+        const description = 'This is a fake consultation description';
+        const consultationDate = '2024-12-21';
+        const startTime = consultationStartTimes[index];
+        const durationMinutes = 30;
+        return {
+          description,
+          consultationDate,
+          startTime,
+          durationMinutes,
+          subjectId,
+          doctorId,
+          authorId,
+          patientId
+        };
+      }
+    );
+
+    const consultationValues = consultations
+      .map(
+        (consultation) =>
+          `('${consultation.description}', '${consultation.consultationDate}', '${consultation.startTime}', ${consultation.durationMinutes}, ${consultation.subjectId}, ${consultation.doctorId}, ${consultation.authorId}, ${consultation.patientId})`
+      )
+      .join(', ');
+
+    const consultationResult = await queryRunner.manager.query(`
+      INSERT INTO "consultation"
+      ("description", "consultationDate", "startTime", "durationMinutes", "subjectId", "doctorId", "authorId", "patientId")
+      VALUES ${consultationValues}
+      RETURNING id
+    `);
+
+    const consultationIds = consultationResult.map(
+      (item: { id: number }) => item.id
+    );
+
+    console.info(consultationIds);
 
     // COMMIT TRANSACTION
     await queryRunner.commitTransaction();

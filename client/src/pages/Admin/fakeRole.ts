c0d387d7687974debd298af3a1e439d48be75574
julien44830 <deniaudjulien0@gmail.com>
2024-12-05 09:30:29 +0100

    feature/client : create fakeRole

---
+++ client/src/pages/Admin/fakeRole.ts
@@ -0,0 +1,20 @@
+const roles: [] = [
+  {
+    id: 1,
+    labelm: 'medecin'
+  },
+  {
+    id: 2,
+    labelm: 'agent'
+  },
+  {
+    id: 3,
+    labelm: 'secretaire'
+  },
+  {
+    id: 4,
+    labelm: 'secretaire'
+  }
+];
+
+export default roles;

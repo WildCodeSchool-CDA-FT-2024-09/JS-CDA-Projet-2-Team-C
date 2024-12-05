c26b053c4f8d3e9d7011e5859454ad41ddbf11d8
Adri-bis <adrien.ma.16@hotmail.fr>
2024-12-05 09:34:20 +0100

    add test for quicksearch

---
+++ client/src/pages/QuickSearch/QuickSearch.test.tsx
@@ -0,0 +1,31 @@
+import { screen, render } from '@testing-library/react';
+import { describe, it, expect } from 'vitest';
+import { MemoryRouter } from 'react-router-dom';
+import '@testing-library/jest-dom';
+import QuickSearch from './QuickSearch';
+
+describe('Test du composant QuickSearch', () => {
+  it('Should render the QuickSearch component with links', () => {
+    render(
+      // Crée un faux router pour simuler les link
+      <MemoryRouter>
+        <QuickSearch />
+      </MemoryRouter>
+    );
+
+    // Le i permet d'accepter les minuscule ou majuscule pour eviter la casse
+    const serviceLink = screen.getByRole('link', { name: /Service/i });
+    const doctorLink = screen.getByRole('link', { name: /Docteur/i });
+    const patientLink = screen.getByRole('link', { name: /Patient/i });
+
+    // Vérifie si les liens affichent le bon texte
+    expect(serviceLink).toHaveTextContent('Service');
+    expect(doctorLink).toHaveTextContent('Docteur');
+    expect(patientLink).toHaveTextContent('Patient');
+
+    // Vérifie si les liens pointent vers les bonnes URLs
+    expect(serviceLink).toHaveAttribute('href', '/rechercher/service');
+    expect(doctorLink).toHaveAttribute('href', '/doctor');
+    expect(patientLink).toHaveAttribute('href', '/patient');
+  });
+});

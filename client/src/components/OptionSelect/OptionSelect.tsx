f80ae65db90e73d2df8372a961e8daa0d97ffe89
julien44830 <deniaudjulien0@gmail.com>
2024-12-05 15:40:53 +0100

    feature/client : refactoring of roleMap for selection option

--- client/src/components/OptionSelect/OptionSelect.tsx
+++ client/src/components/OptionSelect/OptionSelect.tsx
@@ -1,37 +1,18 @@
-import fakeRole from '../../pages/Admin/fakeRole';
-
-const translations = [
-  { en: 'medecin', fr: 'Médecin' },
-  { en: 'agent', fr: 'Agent' },
-  { en: 'secretaire', fr: 'Secrétaire' },
-  { en: 'admin', fr: 'Admin' }
-];
+const roleMap = {
+  agent: 'Agent',
+  secretary: 'Secrétaire',
+  doctor: 'Médecin',
+  admin: 'Admin'
+};
 
 export default function OptionSelect() {
-  // Fonction de traduction
-  const tradRole = (roles) => {
-    return roles.map((role) => {
-      const translation = translations.find((t) => t.en === role.labelm);
-      if (translation) {
-        return { ...role, labelm: translation.fr };
-      }
-      return role; // Retourne le rôle inchangé s'il n'y a pas de traduction
-    });
-  };
-
-  const translatedRoles = tradRole(fakeRole);
-
   return (
-    <select>
-      {translatedRoles.map((role) => (
-        <option key={role.id} value={role.label}>
-          {role.label}
+    <>
+      {Object.entries(roleMap).map(([key, value]) => (
+        <option key={key} value={key}>
+          Role : {value}
         </option>
       ))}
-    </select>
+    </>
   );
-  // <option value="admin">Admin</option>
-  // <option value="agent">Agent</option>
-  // <option value="secretaire">Secrétaire</option>
-  // <option value="medecin">Médecin</option>
 }

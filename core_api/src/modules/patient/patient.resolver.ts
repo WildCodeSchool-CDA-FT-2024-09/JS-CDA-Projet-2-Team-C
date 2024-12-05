b8a5cc5fde2c86c28dc738c93030ee80743196c6
AkiraAcetonoshi <facilitateurmaker@gmail.com>
2024-12-05 15:21:06 +0100

    FIX: fixed the patient resolver

--- core_api/src/modules/patient/patient.resolver.ts
+++ core_api/src/modules/patient/patient.resolver.ts
@@ -1,12 +1,12 @@
-import { Patient } from '../entities.index';
 import { Resolver, Query, Arg } from 'type-graphql';
+import { Patient } from '../entities.index';
 
 @Resolver(Patient)
 export default class PatientResolver {
   // TODO : rescrtict access to role === doctor | secretary
   @Query(() => Patient)
-  async dossier(@Arg('patientId') patientId: number) {
-    return await Patient.find({
+  async patient(@Arg('patientId') patientId: number) {
+    return await Patient.findOne({
       where: { id: patientId },
       relations: {
         gender: true

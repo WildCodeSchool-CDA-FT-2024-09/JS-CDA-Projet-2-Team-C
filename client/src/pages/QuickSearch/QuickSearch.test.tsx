e4d7aa75e9b979bf1cc72fa715de360ddd9c65d3
Adri-bis <adrien.ma.16@hotmail.fr>
2024-12-06 11:27:20 +0100

    fix: delete french comment

--- client/src/pages/QuickSearch/QuickSearch.test.tsx
+++ client/src/pages/QuickSearch/QuickSearch.test.tsx
@@ -7,23 +7,19 @@ import QuickSearch from './QuickSearch';
 describe('Test QuickSearch component', () => {
   it('Should render the QuickSearch component with links', () => {
     render(
-      // Crée un faux router pour simuler les link
       <MemoryRouter>
         <QuickSearch />
       </MemoryRouter>
     );
 
-    // Le i permet d'accepter les minuscules ou majuscules pour eviter la casse
     const serviceLink = screen.getByRole('link', { name: /Service/i });
     const doctorLink = screen.getByRole('link', { name: /Docteur/i });
     const patientLink = screen.getByRole('link', { name: /Patient/i });
 
-    // Vérifie si les liens affichent le bon texte
     expect(serviceLink).toHaveTextContent('Service');
     expect(doctorLink).toHaveTextContent('Docteur');
     expect(patientLink).toHaveTextContent('Patient');
 
-    // Vérifie si les liens pointent vers les bonnes URLs
     expect(serviceLink).toHaveAttribute('href', '/rechercher/service');
     expect(doctorLink).toHaveAttribute('href', '/doctor');
     expect(patientLink).toHaveAttribute('href', '/patient');

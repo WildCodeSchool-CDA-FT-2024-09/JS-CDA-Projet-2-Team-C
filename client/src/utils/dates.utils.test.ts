97cc24f0d4b99eaefe9ec66e34d352eb3e2a8e53
AkiraAcetonoshi <facilitateurmaker@gmail.com>
2024-12-06 09:32:02 +0100

    fix : refactored date tests

--- client/src/utils/dates.utils.test.ts
+++ client/src/utils/dates.utils.test.ts
@@ -2,12 +2,20 @@ import { frenchLargeDate } from './dates.utils';
 import { describe, it, expect } from 'vitest';
 
 describe('frenchLargeDate utility', () => {
-  it('should return a formatted French date string for a valid Date', () => {
+  // handle correct inputs
+  it('should handle a string date input correctly', () => {
+    const inputDate = '2023-12-01';
+    const expectedOutput = '1 décembre 2023';
+    expect(frenchLargeDate(inputDate)).toBe(expectedOutput);
+  });
+
+  it('should handle js Date objects', () => {
     const inputDate = new Date('2023-12-01');
     const expectedOutput = '1 décembre 2023';
     expect(frenchLargeDate(inputDate)).toBe(expectedOutput);
   });
 
+  // handle bad inputs
   it('should return "date inconnue" for an undefined input', () => {
     const inputDate = undefined;
     const expectedOutput = 'date inconnue';
@@ -17,12 +25,6 @@ describe('frenchLargeDate utility', () => {
   it('should handle invalid date input gracefully', () => {
     const inputDate = new Date('invalid-date');
     const expectedOutput = 'date inconnue';
-    expect(frenchLargeDate(inputDate)).toBe(expectedOutput);
-  });
-
-  it('should handle a string date input correctly', () => {
-    const inputDate = '2023-12-01';
-    const expectedOutput = '1 décembre 2023';
     expect(frenchLargeDate(inputDate)).toBe(expectedOutput);
   });
 });

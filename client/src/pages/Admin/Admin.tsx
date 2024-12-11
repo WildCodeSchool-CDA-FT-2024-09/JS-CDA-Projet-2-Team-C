06194f187efe038b57a2abb7d35231baaa8f6b85
julien44830 <deniaudjulien0@gmail.com>
2024-12-11 12:09:49 +0100

    FEATURE/client: fetch back data for display on component admin

--- client/src/pages/Admin/Admin.tsx
+++ client/src/pages/Admin/Admin.tsx
@@ -1,10 +1,12 @@
 import { useState } from 'react';
+import { useUsersQuery } from '../../generated/graphql-types';
 import UserList from '../../components/UserList/UserList';
 import SearchBar from '../../components/SearchBar/SearchBar';
 import OptionSelect from '../../components/OptionSelect/OptionSelect';
-import users from './fakeUsers';
 
 export default function Admin() {
+  const { data, loading, error } = useUsersQuery();
+
   const [searchByName, setSearchByName] = useState<string>('');
   const [role, setRole] = useState<string>('');
 
@@ -12,79 +14,84 @@ export default function Admin() {
     setSearchByName(value);
   };
 
-  // function to filter by name, first name depending on the role
-  const filteredUsers = users.filter(
-    (user) =>
-      user.role.toLowerCase().includes(role.toLowerCase()) &&
-      (user.name.toLowerCase().includes(searchByName.toLowerCase()) ||
-        user.lastName.toLowerCase().includes(searchByName.toLowerCase()))
-  );
+  const filteredUsers =
+    data?.users?.filter(
+      (user) =>
+        user.role.label.toLowerCase().includes(role.toLowerCase()) &&
+        (user.firstname.toLowerCase().includes(searchByName.toLowerCase()) ||
+          user.lastname.toLowerCase().includes(searchByName.toLowerCase()))
+    ) || [];
+
+  if (loading) return <h1>Chargement ...</h1>;
+
+  if (error) return <h1>Erreur</h1>;
 
   // See https://daisyui.com/components/table/ for table component
-  return (
-    <>
-      <section className="h-5/6 min-h-3.5 pl-[15vw] pr-[15vw]">
-        <section className="flex p-[27px]">
-          <div className="basis-1/4">{''}</div>
-          <h2 className="basis-3/4 text-center font-bold">
-            Liste des utilisateurs
-          </h2>
-          <button
-            type="button"
-            className="basis-1/4 rounded-lg bg-primary-dark p-2 text-white hover:bg-secondary"
-          >
-            Ajouter un utilisateur
-          </button>
-        </section>
-        <div className="overflow-x-auto rounded-lg border border-primary-dark p-6">
-          <SearchBar handleChange={handleChange} />
-          <table className="table bg-white">
-            <thead>
-              <tr className="border-b border-gray-300">
-                <th scope="col">
-                  <label htmlFor="role">
-                    <select
-                      id="role"
-                      className="m-[-10px] rounded-lg border border-primary-dark p-2 focus:outline-none"
-                      onChange={(e: React.ChangeEvent<HTMLSelectElement>) =>
-                        setRole(e.target.value)
-                      }
-                    >
-                      <option value="">Roles : tous</option>
-                      <OptionSelect />
-                    </select>
-                  </label>
-                </th>
-                <th scope="col">Nom</th>
-                <th scope="col">Prénom</th>
-                <th scope="col">E-mail</th>
-                <th scope="col" className="w-28">
-                  Action
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              {filteredUsers.map((user) => (
-                <UserList
-                  key={user.id}
-                  id={user.id}
-                  name={user.name}
-                  lastName={user.lastName}
-                  email={user.email}
-                  role={user.role}
-                />
-              ))}
-            </tbody>
-          </table>
-          <section className="w-full text-right">
-            <div className="join">
-              <button className="btn join-item">«</button>
-              <button className="btn join-item">Page 01</button>
-              <button className="btn join-item">»</button>
-            </div>
+  if (data)
+    return (
+      <>
+        <section className="h-5/6 min-h-3.5 pl-[15vw] pr-[15vw]">
+          <section className="flex p-[27px]">
+            <div className="basis-1/4">{''}</div>
+            <h2 className="basis-3/4 text-center font-bold">
+              Liste des utilisateurs
+            </h2>
+            <button
+              type="button"
+              className="basis-1/4 rounded-lg bg-primary-dark p-2 text-white hover:bg-secondary"
+            >
+              Ajouter un utilisateur
+            </button>
           </section>
-        </div>
-      </section>
-    </>
-  );
+          <div className="overflow-x-auto rounded-lg border border-primary-dark p-6">
+            <SearchBar handleChange={handleChange} />
+            <table className="table bg-white">
+              <thead>
+                <tr className="border-b border-gray-300">
+                  <th scope="col">
+                    <label htmlFor="role">
+                      <select
+                        id="role"
+                        className="m-[-10px] rounded-lg border border-primary-dark p-2 focus:outline-none"
+                        onChange={(e: React.ChangeEvent<HTMLSelectElement>) =>
+                          setRole(e.target.value)
+                        }
+                      >
+                        <option value="">Roles : tous</option>
+                        <OptionSelect />
+                      </select>
+                    </label>
+                  </th>
+                  <th scope="col">Nom</th>
+                  <th scope="col">Prénom</th>
+                  <th scope="col">E-mail</th>
+                  <th scope="col" className="w-28">
+                    Action
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                {filteredUsers.map((user) => (
+                  <UserList
+                    key={user.id}
+                    id={user.id}
+                    name={user.firstname}
+                    lastName={user.lastname}
+                    email={user.email}
+                    role={user.role.label}
+                  />
+                ))}
+              </tbody>
+            </table>
+            <section className="w-full text-right">
+              <div className="join">
+                <button className="btn join-item">«</button>
+                <button className="btn join-item">Page 01</button>
+                <button className="btn join-item">»</button>
+              </div>
+            </section>
+          </div>
+        </section>
+      </>
+    );
 }

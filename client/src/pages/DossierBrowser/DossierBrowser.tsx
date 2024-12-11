a621e2ca591be81c60f7f1eecd7cc82f85ac4282
AkiraAcetonoshi <facilitateurmaker@gmail.com>
2024-12-11 15:10:08 +0100

    FEATURE (client/dossier search) added a wrapper around the page content to center it

--- client/src/pages/DossierBrowser/DossierBrowser.tsx
+++ client/src/pages/DossierBrowser/DossierBrowser.tsx
@@ -2,9 +2,12 @@ import PatientSearchBar from '../../components/PatientSearchBar/PatientSearchBar
 
 export default function DossierBrowser() {
   return (
-    <>
-      <h1 className="text-center">Recherche par nom de patient</h1>
-      <PatientSearchBar />
-    </>
+    <div className="flex min-h-[75vh] flex-col items-center justify-center gap-4">
+      <h1 className="text-center">Rechercher un dossier</h1>
+      <p>Entrez le prénom ou le nom d'un patient</p>
+      <PatientSearchBar
+        handlePatientSelected={(patientId) => console.info(patientId)}
+      />
+    </div>
   );
 }

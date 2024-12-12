149b7d633cceabf445166b73a9be48b038c4ae33
AkiraAcetonoshi <facilitateurmaker@gmail.com>
2024-12-12 14:08:54 +0100

    FEATURE added ethereal test

--- mailer/src/index.ts
+++ mailer/src/index.ts
@@ -9,20 +9,17 @@ const app = express();
 app.use(express.json());
 
 app.post('/', async (req, res) => {
-  try{
+  try {
+    const { to, subject, text } = req.body;
+    if (!to || !subject || !text) throw new Error();
 
-    const {to, subject, text}= req.body
-    if (!to || !subject || !text) throw new Error
-
-    const mailOptions = req.body
-    await sendMail(mailOptions)
+    const mailOptions = req.body;
+    await sendMail(mailOptions);
 
     res.sendStatus(201);
   } catch {
-    res.status(400).send("Invalid input")
+    res.status(400).send('Invalid input');
   }
-  
-  
 });
 
 app.listen(serverPort, () => {

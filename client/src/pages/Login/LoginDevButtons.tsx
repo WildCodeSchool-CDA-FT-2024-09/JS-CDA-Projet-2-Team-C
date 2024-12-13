19d2c58339746cdb046d25f1a3a48b440da19e9a
Adri-bis <adrien.ma.16@hotmail.fr>
2024-12-13 11:39:11 +0100

    fix: fix login page and header with new rolelabel changed in the back

--- client/src/pages/Login/LoginDevButtons.tsx
+++ client/src/pages/Login/LoginDevButtons.tsx
@@ -1,6 +1,7 @@
 import { useAuth } from '../../contexts/auth/useAuth';
 import { useNavigate } from 'react-router-dom';
 import { roleLandingPages } from './roleLandingPages';
+import { RoleLabel } from '../../generated/graphql-types';
 
 // This component is used to simulate login for different users roles.
 // It is used in the Login page for development purposes.
@@ -10,7 +11,7 @@ export default function LoginDevButtons() {
   const navigate = useNavigate();
   const { setUser } = useAuth();
 
-  const handleLogin = (role: 'agent' | 'secretary' | 'doctor' | 'admin') => {
+  const handleLogin = (role: RoleLabel) => {
     setUser({
       id: 1,
       firstname: 'John',
@@ -18,35 +19,39 @@ export default function LoginDevButtons() {
       email: 'test@test.com',
       role: {
         id: 1,
-        label: role
+        label: role,
+        users: []
       },
       isArchived: false
     });
-    navigate(roleLandingPages[role]);
+
+    setTimeout(() => {
+      navigate(roleLandingPages[role]);
+    }, 100); // Petit délai pour laisser le contexte se mettre à jour
   };
 
   return (
     <>
       <button
-        onClick={() => handleLogin('agent')}
+        onClick={() => handleLogin(RoleLabel.Agent)}
         className="btn w-40 bg-primary"
       >
         Login Agent
       </button>
       <button
-        onClick={() => handleLogin('secretary')}
+        onClick={() => handleLogin(RoleLabel.Secretary)}
         className="btn w-40 bg-primary"
       >
         Login Secretary
       </button>
       <button
-        onClick={() => handleLogin('doctor')}
+        onClick={() => handleLogin(RoleLabel.Doctor)}
         className="btn w-40 bg-primary"
       >
         Login Doctor
       </button>
       <button
-        onClick={() => handleLogin('admin')}
+        onClick={() => handleLogin(RoleLabel.Admin)}
         className="btn w-40 bg-primary"
       >
         Login Admin

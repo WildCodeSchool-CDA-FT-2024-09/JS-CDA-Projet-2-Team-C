7c6316ea1cace8447620005824c6ea96e0b3d137
Thomas Allen <thomas-allen_student2021@wilder.school>
2024-12-17 17:05:02 +0100

    FIX/Seed: update dev and test seeds with static emails for first user with each role (fakedoctor@fake.com etc.) to give a predictable login for each role. Update consultation seed to create one consultation per patient in the 2 hours following today, now (UTC+1)

--- core_api/src/database/seed.jest.ts
+++ core_api/src/database/seed.jest.ts
@@ -110,7 +110,7 @@ const seed = async () => {
       {
         firstname: 'Cyril',
         lastname: 'Convergence',
-        email: await fakeEmail(),
+        email: 'fakedoctor@fake.com',
         password: 'fake1234hash',
         roleId: roleIdMap.doctor,
         genderId: genderIdMap.male,
@@ -148,7 +148,7 @@ const seed = async () => {
       {
         firstname: 'Arnold',
         lastname: 'Agent',
-        email: await fakeEmail(),
+        email: 'fakeagent@fake.com',
         password: 'fake1234hash',
         roleId: roleIdMap.agent,
         genderId: genderIdMap.na
@@ -181,7 +181,7 @@ const seed = async () => {
       {
         firstname: 'Alice',
         lastname: 'Admin',
-        email: await fakeEmail(),
+        email: 'fakeadmin@fake.com',
         password: 'fake1234hash',
         roleId: roleIdMap.admin,
         genderId: genderIdMap.na
@@ -214,7 +214,7 @@ const seed = async () => {
       {
         firstname: 'Samuel',
         lastname: 'Secretary',
-        email: await fakeEmail(),
+        email: 'fakesecretary@fake.com',
         password: 'fake1234hash',
         roleId: roleIdMap.secretary,
         genderId: genderIdMap.na,
@@ -402,7 +402,19 @@ const seed = async () => {
         consultation2.consultationDate = '2024-11-22';
         const consultation3 = { ...baseConsultation };
         consultation3.consultationDate = '2024-11-13';
-        return [consultation1, consultation2, consultation3];
+        // Create a consultation for today, in the next 2 hours
+        const consultation4 = { ...baseConsultation };
+        consultation4.consultationDate = new Date().toISOString().split('T')[0];
+        const now = new Date();
+        const twoHoursLater = new Date(now.getTime() + 2 * 60 * 60 * 1000);
+        const randomTime = new Date(
+          now.getTime() +
+            Math.random() * (twoHoursLater.getTime() - now.getTime())
+        );
+        randomTime.setMinutes(Math.round(randomTime.getMinutes() / 15) * 15);
+        randomTime.setHours(randomTime.getHours() + 1); // Add one hour to offset from UTC
+        consultation4.startTime = randomTime.toTimeString().slice(0, 5);
+        return [consultation1, consultation2, consultation3, consultation4];
       }
     );
 

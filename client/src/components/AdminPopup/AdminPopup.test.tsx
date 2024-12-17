fe89e4983898b15cebb5a3da678766ad4ac9a141
Thomas Allen <thomas-allen_student2021@wilder.school>
2024-12-17 22:04:42 +0100

    Client/tests: update layout test to use mocked displayText provider. Add test.utils with utility function to provide context provider mocks for tests where context doesn't need specific parameters to be set

--- client/src/components/AdminPopup/AdminPopup.test.tsx
+++ client/src/components/AdminPopup/AdminPopup.test.tsx
@@ -1,4 +1,5 @@
-import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import { screen, fireEvent, waitFor } from '@testing-library/react';
+import { renderWithAllProviders as render } from '../../utils/test/test.utils';
 import { vi } from 'vitest';
 import AdminPopup from './AdminPopup';
 import {

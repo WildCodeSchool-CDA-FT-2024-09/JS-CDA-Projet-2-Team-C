f92a9ff307a3b1b22bd4882ed769f55a3bb1c5ec
AkiraAcetonoshi <facilitateurmaker@gmail.com>
2024-12-19 19:11:38 +0100

    FEATURE (client/secretary) used fetched consultations query in the front end to be triggered by the doctor selector component

--- client/src/pages/SecretaryHome/SecretaryHome.tsx
+++ client/src/pages/SecretaryHome/SecretaryHome.tsx
@@ -1,20 +1,48 @@
+import { useEffect, useState } from 'react';
 import Agenda from '../../components/secretary_components/Agenda/Agenda';
 import DoctorSelector from '../../components/secretary_components/DoctorSelector/DoctorSelector';
 import FormPanel from '../../components/secretary_components/FormPanel/FormPanel';
+import {
+  ConsultationsByDoctorIdQuery,
+  useConsultationsByDoctorIdLazyQuery
+} from '../../generated/graphql-types';
 
 export default function SecretaryHome() {
+  const [doctorId, setDoctorId] = useState<number>(1);
+  const [consultations, setConsultations] = useState<
+    ConsultationsByDoctorIdQuery['consultationsByDoctorId']
+  >([]);
+  const [getConsultationsByDoctorId, { data }] =
+    useConsultationsByDoctorIdLazyQuery();
+
+  useEffect(() => {
+    // the default doctorId is 0
+    if (doctorId) {
+      getConsultationsByDoctorId({ variables: { doctorId: doctorId } });
+      // console.log('fetching consultations', doctorId);
+    }
+  }, [doctorId, getConsultationsByDoctorId]);
+
+  useEffect(() => {
+    if (data) setConsultations(data?.consultationsByDoctorId);
+  }, [data, getConsultationsByDoctorId]);
+
+  // console.log(consultations);
+
   return (
     <div className="grid grid-cols-2 gap-8 p-8">
       <section className="rounded-2xl bg-primary-lighter p-4">
         <FormPanel title={'Médecin'}>
-          <DoctorSelector handleDoctorSelected={(e) => console.info(e)} />
+          <DoctorSelector
+            handleDoctorSelected={(doctor) => setDoctorId(doctor.id)}
+          />
         </FormPanel>
         <FormPanel title={'Patient'}>partie patient</FormPanel>
         <FormPanel title={'Horaire'}>partie motif</FormPanel>
         <FormPanel title={'Motif'}>partie motif</FormPanel>
       </section>
       <section>
-        <Agenda />
+        <Agenda consultations={consultations} />
       </section>
     </div>
   );

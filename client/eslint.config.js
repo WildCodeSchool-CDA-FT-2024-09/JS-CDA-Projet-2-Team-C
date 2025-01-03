bd86c5cecf5ac43824a1882b902a1b6fb9fa1868
AkiraAcetonoshi <facilitateurmaker@gmail.com>
2025-01-03 16:29:59 +0100

    CHORE changed ESLint config to include airbnb style guide

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -2,20 +2,25 @@ import js from '@eslint/js';
 import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
+import react from 'eslint-plugin-react';
+import jsxA11y from 'eslint-plugin-jsx-a11y';
 import tseslint from 'typescript-eslint';
+import airbnb from 'eslint-config-airbnb';
 
 export default tseslint.config(
   { ignores: ['dist', 'src/generated/*.ts'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [js.configs.recommended, ...tseslint.configs.recommended, airbnb],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser
     },
     plugins: {
+      react,
       'react-hooks': reactHooks,
-      'react-refresh': reactRefresh
+      'react-refresh': reactRefresh,
+      jsxA11y
     },
     rules: {
       ...reactHooks.configs.recommended.rules,

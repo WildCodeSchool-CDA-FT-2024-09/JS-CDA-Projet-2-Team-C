44e6a240f5401817082292cda580e2730a324d22
Thomas Allen <thomas-allen_student2021@wilder.school>
2025-01-12 16:29:05 +0100

    FEATURE/auth/session-reload: udpate authContext to query current authUser if no user found, update redirect wrapper to wait for auth check before redirect and display a toast on successful session refresh

--- client/src/contexts/auth/AuthContext.tsx
+++ client/src/contexts/auth/AuthContext.tsx
@@ -1,14 +1,32 @@
-import { createContext, PropsWithChildren, useState } from 'react';
-import { AuthUser } from '../../generated/graphql-types';
+import { createContext, PropsWithChildren, useState, useEffect } from 'react';
+import {
+  AuthUser,
+  useGetCurrentAuthUserLazyQuery
+} from '../../generated/graphql-types';
 import { AuthContextType } from './AuthContext.types';
 
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
 export const AuthProvider = ({ children }: PropsWithChildren) => {
   const [user, setUser] = useState<AuthUser | null>(null);
+  const [authChecked, setAuthChecked] = useState(false);
+  const [getCurrentUser, { data, error }] = useGetCurrentAuthUserLazyQuery();
+
+  useEffect(() => {
+    getCurrentUser();
+  }, [getCurrentUser]);
+
+  useEffect(() => {
+    if (data?.getCurrentAuthUser) {
+      setUser(data.getCurrentAuthUser);
+    }
+    if (data || error) {
+      setAuthChecked(true);
+    }
+  }, [data, error]);
 
   return (
-    <AuthContext.Provider value={{ user, setUser }}>
+    <AuthContext.Provider value={{ user, setUser, authChecked }}>
       {children}
     </AuthContext.Provider>
   );

f836b841a4e2ba2bc676be62df3228bacd9ac80b
Ektoplasme <kainomel@hotmail.fr>
2025-01-17 14:56:10 +0100

    hotfix: secretary result color

--- client/src/components/shared_components/PatientSearchBar/PatientSearchBar.tsx
+++ client/src/components/shared_components/PatientSearchBar/PatientSearchBar.tsx
@@ -37,7 +37,10 @@ export default function PatientSearchBar({
             {data.patients.length ? (
               data.patients.map((patient) => (
                 <li key={`patient-${patient.id}`}>
-                  <button onClick={() => handlePatientSelected(patient.id)}>
+                  <button
+                    className="text-black"
+                    onClick={() => handlePatientSelected(patient.id)}
+                  >
                     <strong>
                       {patient.firstname} {patient.lastname}
                     </strong>

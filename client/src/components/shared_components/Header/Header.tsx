e1862089ad5db8a39269c47262af8c01f0976372
Ektoplasme <kainomel@hotmail.fr>
2025-01-17 15:01:31 +0100

    hotfix: header burger menu color

--- client/src/components/shared_components/Header/Header.tsx
+++ client/src/components/shared_components/Header/Header.tsx
@@ -62,9 +62,9 @@ export const Header = ({ page, pageNames }: HeaderProps) => {
             <svg
               xmlns="http://www.w3.org/2000/svg"
               className="h-5 w-5"
-              fill="none"
+              fill="white"
               viewBox="0 0 24 24"
-              stroke="currentColor"
+              stroke="white"
             >
               <path
                 strokeLinecap="round"

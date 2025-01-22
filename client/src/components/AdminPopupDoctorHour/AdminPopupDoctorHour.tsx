75178d426e0a8d9a156359c7e621679ce9993c8a
AkiraAcetonoshi <facilitateurmaker@gmail.com>
2025-01-22 11:11:40 +0100

    FIX (admin pop up hours) : corrected spelling error

--- client/src/components/AdminPopupDoctorHour/AdminPopupDoctorHour.tsx
+++ client/src/components/AdminPopupDoctorHour/AdminPopupDoctorHour.tsx
@@ -176,7 +176,7 @@ export default function AdminPopupDoctorHour({
             </button>
           </form>
           <h3 className="text-center text-lg font-bold text-primary">
-            horaires du médecin {nameDoctor}
+            Horaires du médecin {nameDoctor}
           </h3>
           <form onSubmit={handleSubmit}>
             <div className="form-control w-full max-w-xs">

eb211c930a3cd68e7ec51aba9b580d236b9a716f
AkiraAcetonoshi <facilitateurmaker@gmail.com>
2025-01-22 11:43:15 +0100

    FEATURE ( doctor planning ) : basic functionnality is ok, created doctorHome and plugged in clendar with the doctor's appointments

---
+++ client/src/pages/DoctorHome/DoctorHome.tsx
@@ -0,0 +1,13 @@
+import Agenda from '../../components/secretary_components/Agenda/Agenda';
+import { useAuth } from '../../contexts/auth/useAuth';
+import { useConsultationsByDoctorIdQuery } from '../../generated/graphql-types';
+
+export default function DoctorHome() {
+  const { user } = useAuth();
+  const { loading, data, error } = useConsultationsByDoctorIdQuery({
+    variables: { doctorId: user!.id } // we're sure that user isn't null because we checked auth before
+  });
+  if (loading) return <p>Chargement de votre planning ...</p>;
+  if (error) return <p>Erreur dans la récupération du plaaning</p>;
+  if (data) return <Agenda consultations={data?.consultationsByDoctorId} />;
+}
